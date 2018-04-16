7016f43973cf59eb2eab5cfa90f42924a251bcd3
Duncan Casteleyn <10881109+DuncanCasteleyn@users.noreply.github.com>
2018-04-16 15:28:39 +0200

    Added views

--- src/main/java/edu/ap/spring/controllers/InhaalExamenController.java
+++ src/main/java/edu/ap/spring/controllers/InhaalExamenController.java
@@ -4,9 +4,8 @@ import edu.ap.spring.model.InhaalExamen;
 import edu.ap.spring.redis.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class InhaalExamenController {
@@ -15,16 +14,23 @@ public class InhaalExamenController {
     RedisService repository;
 
     @RequestMapping("/list")
-    public String list(@RequestParam(name = "student") String student) {
+    public String list(@RequestParam(name = "student") String student, Model model) {
         String content = repository.getKey(student);
-
-        return content;
+        String[] splittedContent = content.split("-");
+        InhaalExamen inhaalExamen = new InhaalExamen(splittedContent[0], splittedContent[1], splittedContent[1]);
+        model.addAttribute( "inhaalExamen", inhaalExamen.toString());
+        return "list";
     }
 
-    @PutMapping("/new")
-    public void newStudent(@RequestParam(name = "student") String student, @RequestParam(name = "exam") String exam, @RequestParam(name = "reason") String reason) {
+    @PostMapping("/new")
+    public String newStudent(@RequestParam("student") String student, @RequestParam("exam") String exam, @RequestParam("reason") String reason, Model model) {
         InhaalExamen inhaalExamen = new InhaalExamen(student, exam, reason);
-        if(repository.getKey(inhaalExamen.getStudent()) != null)
+        if(repository.getKey(inhaalExamen.getStudent()) != null) {
+            model.addAttribute("inhaalExamen", "Already exists");
+            return "new";
+        }
         repository.setKey(inhaalExamen.getStudent(), inhaalExamen.toString());
+        model.addAttribute("inhaalExamen", inhaalExamen.toString());
+        return "new";
     }
 }
